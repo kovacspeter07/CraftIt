45d47bfb5475eb2b21ef7c49df6c725c763b79ca
kovacspeter07 <kovacs.peter1@students.jedlik.eu>
2025-01-30 17:48:20 +0100

    firts working background title change

--- script.js
+++ script.js
@@ -18,4 +18,36 @@ document.getElementById("exit-button").onclick = function() {ExitPage()};
 function ExitPage() {
     window.close()
 }
+// setting button
+var background_index = 0;
+const background_variants = [
+    {
+        title:"Grass",
+        layer_3:"dirt",
+        layer_2:"dirt_path_side",
+        layer_1:"short_grass",
+    },
+    {
+        title:"Sand",
+        layer_3:"sand",
+        layer_2:"sand",
+        layer_1: "",
+    }
+];
+
+document.getElementById("settings-button").onclick = function() {settingsScreen()};
+
+function settingsScreen() {
+    document.getElementById("menu-title").innerHTML = ""
+    document.getElementById("menu-buttons").innerHTML = `<div id="background-button" class="menu-button">Background: Grass</div>`;
+    document.getElementById("background-button").onclick = function() {changeBackground()};
+}
+
+
+
+function changeBackground() {
+    background_index += 1;
+    document.getElementById("background-button").textContent = `Background: ${background_variants[background_index].title}`;
+}
+
 
