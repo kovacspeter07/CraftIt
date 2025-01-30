2620639ad655c4424ff98fbb79a653544f4fbb35
poczemartin <pocze.martin@students.jedlik.eu>
2025-01-30 13:10:35 +0100

    back button functionally working, no css

--- script.js
+++ script.js
@@ -4,11 +4,22 @@ document.getElementById("play-button").onclick = function() {difficultyScreen()}
 function difficultyScreen() {
     document.getElementById("menu-title").innerHTML = "Choose difficulty"
     difficultyButtons()
+    backButton()
 }
 
 function difficultyButtons() {
     document.getElementById("menu-buttons").innerHTML = `<div id="easy-button" class="menu-button">Easy</div>
     <div id="medium-button" class="menu-button">Medium</div>
     <div id="hard-button" class="menu-button">Hard</div>`;
+}
+//use this function to put a back button on the page
+function backButton() {
+    document.getElementById("back-button").innerHTML = `<button>Back</button>`
+}
+
+document.getElementById("back-button").onclick = function() {reloadScreen()};
+
+function reloadScreen() {
+    location.reload();
 }
 //this point forward it's gonna be something else(change this text)
