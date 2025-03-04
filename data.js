3e2a1c3f16fbcb44439eaa9c211401c5d215ea96
majorbalintistvan <major.balint.istvan@students.jedlik.eu>
2025-03-04 20:10:26 +0100

    I think i finished it. For now

--- data.js
+++ data.js
@@ -659,7 +659,9 @@ const items_data = [
       [ null,null, null,null ,"brick", "brick", null,"brick","brick"],
     ],
   },
-  /**Hard*/
+
+  /**Hard (21)*/
+
   { 
     picture: "crafter",
     craftable: true,
@@ -668,7 +670,184 @@ const items_data = [
       ["iron","iron", "iron", "iron", "crafting_table", "iron", "redstone","dropper", "redstone"],
     ],
   },
-  /**ingredients*/ 
+  { 
+    picture: "dropper",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      ["cobblestone","cobblestone", "cobblestone", "cobblestone", null, "cobblestone", "cobblestone","redstone", "cobblestone"],
+    ],
+  },
+  { 
+    picture: "observer",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      ["cobblestone","cobblestone", "cobblestone", "redstone",  "quartz","redstone", "cobblestone","cobblestone", "cobblestone",],
+    ],
+  },
+  { 
+    picture: "beacon",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      ["glass","glass", "glass", "glass", "nether_star", "glass", "obsidian","obsidian", "obsidian"],
+    ],
+  },
+  { 
+    picture: "dispenser",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      ["cobblestone","cobblestone", "cobblestone", "cobblestone", "bow", "cobblestone", "cobblestone","redstone", "cobblestone"],
+    ],
+  },
+  { 
+    picture: "loom",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      ["string","string", null, "oak_planks","oak_planks", null, null,null, null],
+      [ null,null, null,"string","string", null, "oak_planks","oak_planks", null],
+      [ null,"string","string", null, "oak_planks","oak_planks", null,null, null],
+      [ null,null, null,null ,"string", "string", null,"oak_planks","oak_planks"],
+    ],
+  },
+  { 
+    picture: "painting",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      ["stick","stick", "stick", "stick", "wool", "stick", "stick","stick", "stick"],
+    ],
+  },
+  { 
+    picture: "respawn_anchor",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      ["crying_obsidian","crying_obsidian", "crying_obsidian", "glowstone",  "glowstone","glowstone", "crying_obsidian","crying_obsidian", "crying_obsidian",],
+    ],
+  },
+  { 
+    picture: "tinted_glass",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      [null,"amethyst_shard",null,"amethyst_shard","glass","amethyst_shard",null,"amethyst_shard",null],
+    ],
+  },
+  { 
+    picture: "scaffolding",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      ["bamboo","string", "bamboo", "bamboo", null, "bamboo", "bamboo",null, "bamboo"],,
+    ],
+  },
+  { 
+    picture: "lead",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      ["string","string", null, "string", "slime_ball", null,null,null, "string"],
+    ],
+  },
+  { 
+    picture: "brush",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      [null,"feather",null,null,"copper",null,null,"stick",null],
+      ["feather", null,null,"copper", null,null,"stick",null,null],
+      [null,null,"feather", null,null, "copper",null,null,"stick"],
+    ],
+  },
+  { 
+    picture: "recovery_compass",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      ["echo_shard","echo_shard", "echo_shard", "echo_shard", "compass", "echo_shard", "echo_shard","echo_shard", "echo_shard"],
+    ],
+  },
+  { 
+    picture: "cake",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      ["milk_bucket","milk_bucket", "milk_bucket", "sugar", "egg", "sugar", "wheat","wheat", "wheat"],
+    ],
+  },
+  { 
+    picture: "candle",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      ["string", null, null,"honeycomb",null , null,null, null,null],
+      [null,"string",null,null ,"honeycomb", null,null, null,null],
+      [ null, null,"string",null , null,"honeycomb", null,null, null],
+      [null, null,null,"string", null, null,"honeycomb",null , null],
+      [null, null,null,null,"string",null,null ,"honeycomb", null],
+      [null,null, null,null, null,"string",null , null,"honeycomb"],
+    ],
+  },
+  { 
+    picture: "bundle",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      ["string", null, null,"leather",null , null,null, null,null],
+      [null,"string",null,null ,"leather", null,null, null,null],
+      [ null, null,"string",null , null,"leather", null,null, null],
+      [null, null,null,"string", null, null,"leather",null , null],
+      [null, null,null,null,"string",null,null ,"leather", null],
+      [null,null, null,null, null,"string",null , null,"leather"],
+    ],
+  },
+  { 
+    picture: "note_block",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      ["oak_planks","oak_planks", "oak_planks", "oak_planks", "redstone", "oak_planks", "oak_planks","oak_planks", "oak_planks"],
+    ],
+  },
+  { 
+    picture: "wolf_armor",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      ["armadilo_scute",null, null, "armadilo_scute", "armadilo_scute", "armadilo_scute", "armadilo_scute",null, "armadilo_scute"],
+    ],
+  },
+  { 
+    picture: "end_crystal",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      ["glass","glass", "glass", "glass", "ender_eye", "glass", "glass","ghast_tear", "glass"],
+    ],
+  },
+  { 
+    picture: "lodestone",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      ["chiseled_stone_bricks","chiseled_stone_bricks", "chiseled_stone_bricks", "chiseled_stone_bricks", "netherite", "chiseled_stone_bricks", "chiseled_stone_bricks","chiseled_stone_bricks", "chiseled_stone_bricks"],
+    ],
+  },
+  { 
+    picture: "sea_lanter",
+    craftable: true,
+    difficulty: "hard",
+    recipe: [
+      ["prismarine_shard","prismarine_crystal", "prismarine_shard", "prismarine_crystal", "prismarine_crystal", "prismarine_crystal", "prismarine_shard","prismarine_crystal", "prismarine_shard"],
+    ],
+  },
+
+  /**ingredients*/
+
   {
     picture: "coal",
     craftable: false,
@@ -712,6 +891,18 @@ const items_data = [
     recipe: null,
   },
   {
+    picture: "quartz",
+    craftable: false,
+    difficulty:undefined,
+    recipe: null,
+  },
+  {
+    picture: "nether_star",
+    craftable: false,
+    difficulty:undefined,
+    recipe: null,
+  },
+  {
     picture: "stick",
     craftable: false,
     difficulty: undefined,
@@ -743,6 +934,18 @@ const items_data = [
   },
   {
     picture: "obsidian",
+    craftable: false,
+    difficulty: undefined,
+    recipe: null,
+  },
+  {
+    picture: "prismarine_crystal",
+    craftable: false,
+    difficulty: undefined,
+    recipe: null,
+  },
+  {
+    picture: "prismarine_shard",
     craftable: false,
     difficulty: undefined,
     recipe: null,
@@ -820,6 +1023,24 @@ const items_data = [
     recipe: null,
   },
   {
+    picture: "egg",
+    craftable: false,
+    difficulty:undefined,
+    recipe: null,
+  },
+  {
+    picture: "sugar",
+    craftable: false,
+    difficulty:undefined,
+    recipe: null,
+  },
+  {
+    picture: "milk_bucket",
+    craftable: false,
+    difficulty:undefined,
+    recipe: null,
+  },
+  {
     picture: "iron_nugget",
     craftable: false,
     difficulty:undefined,
@@ -838,6 +1059,12 @@ const items_data = [
     recipe: null,
   },
   {
+    picture: "armadilo_scute",
+    craftable: false,
+    difficulty:undefined,
+    recipe: null,
+  },
+  {
     picture: "feather",
     craftable: false,
     difficulty:undefined,
@@ -845,6 +1072,12 @@ const items_data = [
   },
   {
     picture: "slime_ball",
+    craftable: false,
+    difficulty:undefined,
+    recipe: null,
+  },
+  {
+    picture: "bamboo",
     craftable: false,
     difficulty:undefined,
     recipe: null,
@@ -867,7 +1100,48 @@ const items_data = [
     difficulty:undefined,
     recipe: null,
   },
-
+  {
+    picture: "wool",
+    craftable: false,
+    difficulty:undefined,
+    recipe: null,
+  },
+  {
+    picture: "crying_obsidian",
+    craftable: false,
+    difficulty:undefined,
+    recipe: null,
+  },
+  {
+    picture: "glowstone",
+    craftable: false,
+    difficulty:undefined,
+    recipe: null,
+  },
+  {
+    picture: "echo_shard",
+    craftable: false,
+    difficulty:undefined,
+    recipe: null,
+  },
+  {
+    picture: "ender_eye",
+    craftable: false,
+    difficulty:undefined,
+    recipe: null,
+  },
+  {
+    picture: "ghast_tear",
+    craftable: false,
+    difficulty:undefined,
+    recipe: null,
+  },
+  {
+    picture: "chiseled_stone_bricks",
+    craftable: false,
+    difficulty:undefined,
+    recipe: null,
+  },
 
 
 ];
